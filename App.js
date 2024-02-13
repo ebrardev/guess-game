55b022675993795b888610b605129272df1a1009
ebrardev <16155052@mersin.edu.tr>
2024-02-13 20:08:58 +0300

    linear gradien expo packet

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
-
+import  {LinearGradient} from 'expo-linear-gradient';
 export default function App() {
   return (
-  <View style={styles.rootContainer}>
+  <LinearGradient colors={["#260078","#06c2b5", "#ed05bb"]} style={styles.rootContainer}>
   
   
   <StartGameScreen/>
-  </View>
+  </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootContainer: {
-    backgroundColor:'#6e34eb',
+    
     flex:1,
-    justifyContent:' space-between',
+   
   },
 });
