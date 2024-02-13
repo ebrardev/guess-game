96edf0d2ac4a772249bb243f3c272ef689d63cca
ebrardev <16155052@mersin.edu.tr>
2024-02-13 14:24:43 +0300

    startgameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,32 @@
 
-import { TextInput,Button, View } from "react-native";
+import { TextInput,Button, View,StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
     return (
-    <View>
+    <View style={styles.inputContainer}> 
         <TextInput />
-
+   <PrimaryButton>Reset </PrimaryButton>
+    <PrimaryButton>Confirm </PrimaryButton>
     </View>
     )
 }
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+inputContainer : {
+   
+    padding: 16,
+    marginTop:100,
+    marginHorizontal: 24,
+    backgroundColor:'#72063c',
+    color:'white',
+    borderRadius:8,
+    elevation:4,
+    shadowColor:'black',
+    shadowOffset:{width:0,height:2},
+    shadowRadius:6,
+    shadowOpacity:0.25,
+
+}
+})
