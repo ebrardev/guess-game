a91f312781310a4d0d9ee7c99bb11d67ed324727
ebrardev <16155052@mersin.edu.tr>
2024-02-13 13:27:50 +0300

    started

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+
+import { TextInput,Button, View } from "react-native";
+
+function StartGameScreen() {
+    return (
+    <View>
+        <TextInput />
+
+    </View>
+    )
+}
+export default StartGameScreen;
