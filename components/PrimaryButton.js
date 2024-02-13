9effbba64a7d0f74f5c4639018ffb988655bb95c
ebrardev <16155052@mersin.edu.tr>
2024-02-13 18:22:06 +0300

    pressable style

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,12 +1,42 @@
 
 
-import { View,Text } from "react-native";
+import { View,Text ,Pressable,StyleSheet} from "react-native";
 
 function PrimaryButton({children}) {
+ function onPressHandler(){
+        console.log('Button pressed');
+    }
+
     return (
-        <View>
-            <Text>{children}</Text>
+     <Pressable
+      onPress={onPressHandler} 
+      style={({pressed}) => pressed ? [ styles.buttonInnerContainer ,styles.pressed] :  styles.buttonInnerContainer}
+      android_ripple={{color:"#969e05"}}>
+           <View style={styles.container}>
+            <Text style={styles.text}>{children}</Text>
         </View>
+     </Pressable>
     )
 }
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:'yellow',
+        padding:16,
+        marginVertical:8,
+        borderRadius:8,
+        elevation:2,
+        shadowColor:'black',
+        shadowOffset:{width:0,height:2},
+        shadowRadius:6,
+        shadowOpacity:0.25,
+
+    },
+    text: {
+        color:'black',
+        fontWeight:'bold',
+        textAlign:'center'
+    }
+
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
     return (
     <View style={styles.inputContainer}> 
-        <TextInput  style={styles.numberInput}  maxLength={2}/>
+        <TextInput  style={styles.numberInput} 
+         maxLength={2} 
+         keyboardType="number-pad"
+         autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="31"
+         />
    <PrimaryButton>Reset </PrimaryButton>
     <PrimaryButton>Confirm </PrimaryButton>
     </View>
@@ -35,7 +41,7 @@ numberInput :{
     fontSize:32,
     borderBottomColor:'#ddb52f',
     borderBottomWidth:2,
-    color:'#ddb52f',
+    color:'white',
     marginVertical:8,
     fontWeight:'bold',
    textAlign:'center'
