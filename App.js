fe5f03dbc3d06d415547df551919d5c1bc261d20
ebrardev <16155052@mersin.edu.tr>
2024-02-14 22:09:29 +0300

    switcinh screens

--- App.js
+++ App.js
@@ -2,7 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import  {LinearGradient} from 'expo-linear-gradient';
+import { useState } from 'react';
+import GameScreen from './screens/GameScreen';
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  function pickedNumberHandler(pickedNumber){
+    setUserNumber(pickedNumber);
+  }
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+  if(userNumber){
+    screen = <GameScreen  onPickNumber = {pickedNumberHandler} />
+  }
   return (
   <LinearGradient colors={["#260078","#06c2b5", "#ed05bb"]} style={styles.rootContainer}>
   <ImageBackground source={require('./assets/images/bgg.png')}  
@@ -10,8 +21,9 @@ export default function App() {
   resizeMode='cover' 
   imageStyle={{opacity:0.30}}
   >
-  <StartGameScreen/>
+  {screen}
   </ImageBackground>
+
   
 
   </LinearGradient>
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import { TextInput,Button, View,StyleSheet,Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import React, { useState } from "react";
 
-function StartGameScreen() {
+function StartGameScreen({onPickNumber}) {
   const [enteredValue, setEnteredValue] = useState('');
 
   function numberInputHandler(inputText){
@@ -14,10 +14,12 @@ function StartGameScreen() {
     }
     function confirmInputHandler(){
       const chosenNumber = parseInt(enteredValue);
-        if(chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99){
+        if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99){
             Alert.alert('Invalid number!','Number has to be a number between 1 and 99.',[{text:'Okay',style:'destructive',onPress:resetInputHandler}]);
-            return ;
+            return;
         }
+        onPickNumber(chosenNumber);
+        console.log(onPickNumber);
     }
     return (
     <View style={styles.inputContainer}> 
